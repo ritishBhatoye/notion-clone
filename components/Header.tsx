3df7319a14ea2df42803b9e3cdab94afcaec6577
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-01-04 11:56:38 +0530

    STYLE HEADER

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 
-import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react';
 import { useUser } from '@clerk/nextjs';
 import React from 'react'
 
 export const Header = () => {
     const {user}=useUser ();
   return (
-    <div className=''>
+    <div className='flex items-center justify-between p-5'>
         {user && (
-            <h1>{user?.firstName} {`'s`} Space
+            <h1 className='text-2xl'>{user?.firstName} {`'s`} Space
             </h1>
         )}
 {/* Breadcrums */}
@@ -19,7 +19,11 @@ export const Header = () => {
         <SignInButton/>
     </SignedOut>
     <SignedIn>
+        <div className='flex items-center'>
+        <UserButton/>
+    
          <SignOutButton/>
+</div>
     </SignedIn>
 </div>
     </div>
