191eb95787014a969663ed48bc2f21d15e5e1f97
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-01-04 13:47:49 +0530

    SIDEBAR INTEGRATE TO LAYOUT

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Header } from "@/components/Header";
+import { Sidebar } from "@/components/Sidebar";
 
 
 export default function RootLayout({
@@ -19,6 +20,8 @@ export default function RootLayout({
 
         <div className="flex min-h-screen">
 {/* SideBar */}
+
+<Sidebar/>
 <div className="flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide">
 {children} 
 </div>
