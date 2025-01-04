67945aea62f96aeab98ee6d720205ee2bd264202
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-01-04 15:52:40 +0530

    SIDEBAR CHANGE

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,41 +1,46 @@
 import React from 'react'
 import { NewDocumentButton } from './NewDocumentButton'
 import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-  } from "@/components/ui/drawer"
-import { Button } from './ui/button'
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import { MenuIcon } from 'lucide-react'
   
 export const Sidebar = () => {
+    const menuOptions=(
+        <>
+        <NewDocumentButton/>
+
+        </>
+    );
   return (
-    <div className='p-2 md:p-5 bg-gray-2 00 relative'>
-        <div className='hidden md:inline'>
+    <div className='p-2 md:p-5 bg-gray-200 relative'>
 
-        <Drawer>
-  <DrawerTrigger>Open</DrawerTrigger>
-  <DrawerContent>
-    <DrawerHeader>
-      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-      <DrawerDescription>This action cannot be undone.</DrawerDescription>
-    </DrawerHeader>
-    <DrawerFooter>
-      <Button>Submit</Button>
-      <DrawerClose>
-        <Button variant="outline">Cancel</Button>
-      </DrawerClose>
-    </DrawerFooter>
-  </DrawerContent>
-</Drawer>
-
+            <div className='md:hidden'>
+        <Sheet>
+  <SheetTrigger>
+    <MenuIcon 
+    className='p-2 hover:opacity-30 rounded-lg'
+size={40}
+/>
+  </SheetTrigger>
+  <SheetContent side='left'>
+    <SheetHeader>
+      <SheetTitle>Menu</SheetTitle>
+      {/* Options */}
+      <div >{menuOptions }</div>
+     
+    </SheetHeader>
+  </SheetContent>
+</Sheet> 
+</div>
+<div className='md:inline'> {menuOptions}</div>
         </div>
-<NewDocumentButton/>
-    </div>
+
   )
 }
 
