639f125eea53c6746263933fe7dc75a6d91a63f2
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-01-04 15:10:33 +0530

    ADD DRAWER COMPONENT

---
+++ components/NewDocumentButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+export const NewDocumentButton = () => {
+  return (
+    <Button >
+      New Document 
+    </Button>
+  )
+}
+
+
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import { NewDocumentButton } from './NewDocumentButton'
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@/components/ui/drawer"
+import { Button } from './ui/button'
+  
 export const Sidebar = () => {
   return (
-    <div>
-      <h1>SideBar</h1>
+    <div className='p-2 md:p-5 bg-gray-2 00 relative'>
+        <div className='hidden md:inline'>
+
+        <Drawer>
+  <DrawerTrigger>Open</DrawerTrigger>
+  <DrawerContent>
+    <DrawerHeader>
+      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+      <DrawerDescription>This action cannot be undone.</DrawerDescription>
+    </DrawerHeader>
+    <DrawerFooter>
+      <Button>Submit</Button>
+      <DrawerClose>
+        <Button variant="outline">Cancel</Button>
+      </DrawerClose>
+    </DrawerFooter>
+  </DrawerContent>
+</Drawer>
+
+        </div>
+<NewDocumentButton/>
     </div>
   )
 }
